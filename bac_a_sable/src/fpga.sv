5d96847d246a70332ebf856c7ae6a42ee0b2a426
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-06-02 11:39:26 +0200

    chenillard

--- bac_a_sable/src/fpga.sv
+++ bac_a_sable/src/fpga.sv
@@ -27,37 +27,16 @@ module fpga (
    gene_reset gene_reset(.clk(clock_50), .key(key[0]), .reset_n(reset_n));
 	
    // ajouter votre code à partir d'ici
-   
-	logic [7:0] sortie;
-	logic [25:0] cpt;  // 2^26 => 64M > 50_000_000
-	logic [1:0]push_inc_etat;		
-	logic [1:0]push_dec_etat;		
+	logic [9:0] sortie;
+	logic [25:0] cpt;  // 2^26 => 64M > 50_000_000	
 	logic clk_div;
 	logic mode;
-	logic inc;
-	logic dec;	
+	logic g_a_d;
 	
-	// choisir le mode avec le swtich sw[0]
-	always @(*) mode <= sw[0];
-	
-	// Sauvegarde de l'état actuel et de l'état précédent et permet de decaler automatiquement
-	always @(posedge clock_50)
-	begin
-		push_inc_etat <= {push_inc_etat[0], key[1]};
-	push_dec_etat <= {push_dec_etat[0], key[2]};
-
-	end
-	
-	// mise a jour des boutons inc et dec lors d'un front montant
-	always@(*)
-	begin
-	   inc <= !push_inc_etat[0] && push_inc_etat[1];
-		dec <= !push_dec_etat[0] && push_dec_etat[1];
-	end
 	
 	// Compteur (diviseur de freq)
 	always @(posedge clock_50)
-	if(cpt == 50_000_000 -1) 
+	if(cpt == 25_000_000 -1) 
 		begin
 		cpt <= 0;
 		clk_div <= 1;
@@ -68,23 +47,29 @@ module fpga (
 		clk_div <= 0;
 		end
 	
+	
 	always @(posedge clock_50)
 	if(!reset_n)
-			sortie <=  8'd0;
-	else		
-		if (mode) 
+	begin
+			sortie <=  10'd1;
+			g_a_d <= 0;
+	end
+	else				
+		if (clk_div) 
+			if(!g_a_d)
 			begin
-				if (clk_div) sortie <= sortie + 1;	// Compteur (auto)
-			end	
-		else
+				if (sortie[8]) g_a_d <= 1;
+				sortie <= sortie << 1;
+			end
+			else
 			begin
-				if (inc)	sortie <= sortie + 1;
-				if (dec)	sortie <= sortie - 1;
-			end	 
+				if (sortie[1]) g_a_d <= 0;
+				sortie <= sortie >> 1;
+	      end
 	
 	always @(*) ledr <= sortie;
 	
 	dec7seg dec0(.i(sortie[3:0]),.o(hex0));
 	dec7seg dec1(.i(sortie[7:4]),.o(hex1));
 	
-endmodule
+endmodule 
