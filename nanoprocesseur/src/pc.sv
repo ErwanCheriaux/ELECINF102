5f516ac167930cdb4e30260514b65baa9b2595a9
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-06-10 09:28:53 +0200

    modification du bloc PC

--- nanoprocesseur/src/pc.sv
+++ nanoprocesseur/src/pc.sv
@@ -7,16 +7,28 @@
  */
 
 module REG_PC(	input logic clk,
-				input logic reset_n,
-				input logic inc_PC,
-				input logic load_PC,
-				input logic [7:0] data_in,
-				output logic [7:0] PC
+		input logic 	   reset_n,
+		input logic 	   inc_PC,
+		input logic 	   load_PC,
+		input logic [7:0]  data_in,
+		output logic [7:0] PC
                 );
 
   // Ajoutez votre code d'ici
-  
-  
+
+   
+   
+   always @(posedge clk or negedge reset_n)
+     if(!reset_n)
+       PC <= 0;
+     else
+       begin
+       if(load_PC)
+	 PC <= data_in;
+       if(inc_PC)
+	 PC <= PC + 1;
+       end
+   
   
   // A là...
    
